6a3be3297fd0f62f9c5a631adbcdb918a4060ee2
Vixtor120 <victorhidsan@campus.monlau.com>
2025-03-22 00:37:57 +0100

    navbar footer mejoras en general

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import { Menu, X, Bell, Home, Headphones, Mail, Radio, Youtube, Settings, ChevronDown } from 'lucide-react';
+import { Menu, X, Bell, Home, Headphones, Mail, Radio, Youtube, Settings} from 'lucide-react';
 import { motion, AnimatePresence } from 'framer-motion';
 
 function Navbar() {
